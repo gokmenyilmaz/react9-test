7de35f2aa49e7aca13938e63cdc8907882530e7c
gokmen.yilmaz <gokmen1977@gmail.com>
2020-03-18 02:42:33 +0300

    Updates

---
+++ precache-manifest.65cfe2c71c8fe2a6f3d13dee498d9e8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6244ac2478c256a0e9e4fa77752a4f6e",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "28fca2345b9d59547b13",
+    "url": "/react1/static/css/2.9a2cc0a4.chunk.css"
+  },
+  {
+    "revision": "525c585342808edb8ef6",
+    "url": "/react1/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "28fca2345b9d59547b13",
+    "url": "/react1/static/js/2.e0f43f4b.chunk.js"
+  },
+  {
+    "revision": "2d6d9ff39d9fb1470195f5fd5bc24fa7",
+    "url": "/react1/static/js/2.e0f43f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525c585342808edb8ef6",
+    "url": "/react1/static/js/main.e83fd497.chunk.js"
+  },
+  {
+    "revision": "8458cd5984e33d39edd8",
+    "url": "/react1/static/js/runtime-main.8ad5742a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react1/static/media/logo.ee7cd8ed.svg"
+  }
+]);
